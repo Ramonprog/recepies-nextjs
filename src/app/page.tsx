d0b10a30d2eebaf0732a9b1d23355361f729d50e
Ramonprog <programador.ramon@gmail.com>
2025-07-09 15:14:22 -0300

    feat: estrututura da sec hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-   <main className="flex-grow">inicio</main>
+   <main className="flex-grow">
+    <div className="container mx-auto">
+      <section>
+        <h1>Receitas deliciosas</h1>
+        <p>Descubra receitas deliciosas e saborosas para você fazer um bom uso de sua vida.</p>
+        <Link className="bg-red-900 text-white px-4 py-2 rounded-md" href="/receitas">
+          Ver todas as receitas
+        </Link>
+      </section>
+    </div>
+   </main>
   );
 }
