9a495bbd402c5746010a38ca9a2f0e749698e850
Ramonprog <programador.ramon@gmail.com>
2025-07-09 14:51:36 -0300

    feat: adicionando estilos ao header

--- src/components/Headers/index.tsx
+++ src/components/Headers/index.tsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 export function Headers() {
   return (
-    <header className="flex items-center justify-between py-4 px-8 bg-white">
-      <Link href="/">
-        Receitas deliciosas
-      </Link>
-      <nav>
-        <Link href="/">Início</Link>
-        <Link href="/receitas">Receitas</Link>
-      </nav>
+    <header className="py-4 px-8 bg-white text-black ">
+      <div className="container mx-auto flex w-full justify-between">
+        <Link className="text-xl font-bold" href="/">
+          Receitas deliciosas
+        </Link>
+        <nav className="flex gap-4">
+          <Link className="hover:text-red-900 transition-colors" href="/">Início</Link>
+          <Link className="hover:text-red-900 transition-colors" href="/receitas">Receitas</Link>
+        </nav>
+      </div>
     </header>
   );
 }
