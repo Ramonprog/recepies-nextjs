a902923fe16ba85d1ab00911baff74a68af458aa
Ramonprog <programador.ramon@gmail.com>
2025-07-09 15:23:17 -0300

    feat: adiciona section Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-   <main className="flex-grow">
-    <div className="container mx-auto">
-      <section>
-        <h1>Receitas deliciosas</h1>
-        <p>Descubra receitas deliciosas e saborosas para você fazer um bom uso de sua vida.</p>
-        <Link className="bg-red-900 text-white px-4 py-2 rounded-md" href="/receitas">
-          Ver todas as receitas
-        </Link>
+    <main className="flex-grow">
+      <section className="bg-orange-50">
+        <div className="container mx-auto flex flex-col items-center justify-center gap-4 py-16 px-8">
+          <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
+          <p className='text-xl'>Descubra receitas deliciosas e saborosas para você fazer um bom uso de sua vida.</p>
+          <Link className="bg-orange-500 text-white px-4 py-2 rounded-md" href="/receitas">
+            Ver todas as receitas
+          </Link>
+        </div>
       </section>
-    </div>
-   </main>
+    </main>
   );
 }
--- src/components/Headers/index.tsx
+++ src/components/Headers/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Headers() {
   return (
-    <header className="py-4 px-8 bg-white text-black ">
+    <header className="py-4 px-8 bg-white text-black border-b border-gray-200 shadow-md">
       <div className="container mx-auto flex w-full justify-between">
         <Link className="text-xl font-bold" href="/">
           Receitas deliciosas
