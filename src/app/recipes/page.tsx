de3851c81e10ed47faa825bf751235d6a3957c84
Ramonprog <programador.ramon@gmail.com>
2025-07-13 13:17:54 -0300

    feat: cria página de receitas e componente RecipesCard

---
+++ src/app/recipes/page.tsx
@@ -0,0 +1,17 @@
+import { RecipesCard } from "@/components/RecipesCard";
+import { recipes } from "@/lib/data";
+
+export default function RecipesPage() {
+  return (
+    <main className="flex-grow py-8">
+      <div className="cotainer mx-auto">
+        <h1 className="text-3xl font-bold">Recipes</h1>
+        <div className="grid grid-cols-3 gap-8 mt-8">
+        {recipes.map((recipe) => (
+          <RecipesCard key={recipe.id} recipe={recipe} />
+        ))}
+       </div>
+      </div>
+    </main>
+  )
+}
---
+++ src/components/RecipesCard/index.tsx
@@ -0,0 +1,19 @@
+import { Recipe } from "@/@types/recipe";
+import Image from "next/image";
+import Link from "next/link";
+
+export function RecipesCard({recipe}:{recipe:Recipe}) {
+  return (
+    <Link href="#">
+      <div className="border border-slate-200 rounded-lg  overflow-hidden shadow-sm hover:shadow-md transition-shadow">
+        <div className="relative h-48 w-full">
+          <Image src={recipe.image} alt={recipe.title} fill className="object-cover" />
+        </div>
+        <div className="flex flex-col p-4 gap-2">
+          <h3 className="text-lg font-bold">{recipe.title}</h3>
+          <p>Descrição</p>
+        </div>
+      </div>
+    </Link>
+  )
+}
