5000399ac1b923b5a1fe6b3e6bce733b1ff2fba3
Ramonprog <programador.ramon@gmail.com>
2025-07-13 13:46:58 -0300

    feat: adiciona exibição de receitas em destaque na página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
+import { RecipesCard } from "@/components/RecipesCard";
+import { recipes } from "@/lib/data";
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
 
 export default function Home() {
+  const featuredRecipes = recipes.slice(0, 3);
+
   return (
     <main className="flex-grow">
       <section className="bg-orange-50">
         <div className="container mx-auto flex flex-col items-center justify-center gap-4 py-16 px-8">
           <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
           <p className='text-xl'>Descubra receitas deliciosas e saborosas para você fazer um bom uso de sua vida.</p>
-          <Link className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-800 transition-colors" href="/receitas">
+          <Link className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-800 transition-colors" href="/recipes">
             Ver todas as receitas
           </Link>
         </div>
@@ -18,8 +22,13 @@ export default function Home() {
       <section className="py-12">
         <div className="container mx-auto flex flex-col items-center">
           <h2 className="text-3xl font-bold">Receba uma receita de qualidade</h2>
-            {/* TODO: card de receitas */}
-          <Link href="/receitas" className="flex items-center text-orange-400 hover:text-orange-800 transition-colors">Ver todas as receitas <ChevronRight size={16}/></Link>
+          <div className="grid grid-cols-3 gap-8 mt-8">
+            {featuredRecipes.map((recipe) => (
+              <RecipesCard key={recipe.id} recipe={recipe} />
+            ))}
+          </div>
+
+          <Link href="/recipes" className="flex items-center text-orange-400 hover:text-orange-800 transition-colors">Ver todas as receitas <ChevronRight size={16} /></Link>
         </div>
       </section>
     </main>
