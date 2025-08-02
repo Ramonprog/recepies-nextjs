a48afcc891ea77f616a4d55e9177082ba7a992d7
Ramonprog <programador.ramon@gmail.com>
2025-08-02 12:14:04 -0300

    feat: estrutura basica do modal

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,4 +1,4 @@
-
+import { RecipeFormModal } from "@/components/RecipeFormModal";
 import { RecipesCard } from "@/components/RecipesCard";
 import { recipes } from "@/lib/data";
 
@@ -6,13 +6,16 @@ export default function RecipesPage() {
   return (
     <main className="flex-grow py-8">
       <div className="container mx-auto">
-        <h1 className="text-3xl font-bold">Recipes</h1>
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Recipes</h1>
+          <RecipeFormModal />
+        </div>
         <div className="grid grid-cols-3 gap-8 mt-8">
-        {recipes.map((recipe) => (
-          <RecipesCard key={recipe.id} recipe={recipe} />
-        ))}
-       </div>
+          {recipes.map((recipe) => (
+            <RecipesCard key={recipe.id} recipe={recipe} />
+          ))}
+        </div>
       </div>
     </main>
-  )
+  );
 }
---
+++ src/components/RecipeFormModal/index.tsx
@@ -0,0 +1,31 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import { Plus } from "lucide-react";
+
+export function RecipeFormModal() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="cursor-pointer">
+          Nova receita <Plus size={16} />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
