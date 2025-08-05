c829846f3b6b126edd6a6a0840387b70905cb470
Ramonprog <programador.ramon@gmail.com>
2025-08-05 18:58:24 -0300

    feat: adiciona validação de formulário com yup no modal de receita

--- src/components/RecipeFormModal/index.tsx
+++ src/components/RecipeFormModal/index.tsx
@@ -13,14 +13,20 @@ import { Input } from "../ui/input";
 import { useForm } from "react-hook-form";
 import { Textarea } from "../ui/textarea";
 import { useState } from "react";
+import { RecipeFormValues, recipeSchema } from "@/lib/formValidationSchemas";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export function RecipeFormModal() {
   const [open, setOpen] = useState(false);
   const {
     handleSubmit,
-    formState: {},
-  } = useForm<FormData>();
-  const onSubmit = handleSubmit((data) => console.log(data));
+    register,
+    formState: { errors },
+  } = useForm<RecipeFormValues>({
+    resolver: yupResolver(recipeSchema),
+    mode: "onSubmit",
+  });
+  const onSubmit = handleSubmit((data: RecipeFormValues) => console.log(data));
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -38,11 +44,29 @@ export function RecipeFormModal() {
           <div className="grid grid-cols-2 gap-4">
             <div className="">
               <Label htmlFor="title">Título</Label>
-              <Input placeholder="Bolo de cenoura" type="text" id="title" />
+              <Input
+                placeholder="Bolo de cenoura"
+                type="text"
+                id="title"
+                {...register("title")}
+              />
+              {errors?.title && (
+                <p className="text-sm text-red-500">{errors.title.message}</p>
+              )}
             </div>
             <div className="">
               <Label htmlFor="category">Categoria</Label>
-              <Input type="text" id="category" placeholder="Bolos" />
+              <Input
+                type="text"
+                id="category"
+                placeholder="Bolos"
+                {...register("category")}
+              />
+              {errors?.category && (
+                <p className="text-sm text-red-500">
+                  {errors.category.message}
+                </p>
+              )}
             </div>
           </div>
 
@@ -51,26 +75,68 @@ export function RecipeFormModal() {
             <Textarea
               id="description"
               placeholder="Digite a descrição da receita"
+              {...register("description")}
             />
+            {errors?.category && (
+              <p className="text-sm text-red-500">{errors.category.message}</p>
+            )}
           </div>
 
           <div>
             <Label htmlFor="imageUrl">URL</Label>
-            <Input id="imageUrl" placeholder="Cole a a URL da receita" />
+            <Input
+              id="imageUrl"
+              placeholder="Cole a a URL da receita"
+              {...register("imageUrl")}
+            />
+            {errors?.imageUrl && (
+              <p className="text-sm text-red-500">{errors.imageUrl.message}</p>
+            )}
           </div>
 
           <div className="grid grid-cols-3 gap-2">
             <div className="">
               <Label htmlFor="prepTime">Tempo de preparo</Label>
-              <Input placeholder="40min" type="text" id="prepTime" />
+              <Input
+                placeholder="40min"
+                type="text"
+                id="prepTime"
+                {...register("prepTime")}
+              />
+              {errors?.prepTime && (
+                <p className="text-sm text-red-500">
+                  {errors.prepTime.message}
+                </p>
+              )}
             </div>
             <div className="">
               <Label htmlFor="cookTime">Tempo de cozimento</Label>
-              <Input type="text" id="cookTime" placeholder="20min" />
+              <Input
+                type="text"
+                id="cookTime"
+                placeholder="20min"
+                {...register("cookTime")}
+              />
+              {errors?.cookTime && (
+                <p className="text-sm text-red-500">
+                  {errors.cookTime.message}
+                </p>
+              )}
             </div>
             <div className="">
               <Label htmlFor="servings">Porções</Label>
-              <Input type="number" id="servings" placeholder="4" />
+              <Input
+                type="number"
+                id="servings"
+                placeholder="4"
+                defaultValue={1}
+                {...register("servings")}
+              />
+              {errors?.servings && (
+                <p className="text-sm text-red-500">
+                  {errors.servings.message}
+                </p>
+              )}
             </div>
           </div>
 
--- src/lib/formValidationSchemas.ts
+++ src/lib/formValidationSchemas.ts
@@ -4,11 +4,13 @@ export const recipeSchema = yup.object().shape({
   title: yup.string().required("Título é obrigatório"),
   category: yup.string().required("Categoria é obrigatória"),
   description: yup.string().required("Descrição é obrigatória"),
-  imageUrl: yup.string().url("URL inválida").required("URL é obrigatória"),
+  imageUrl: yup.string().required("URL é obrigatória"),
   prepTime: yup.string().required("Tempo de preparo é obrigatório"),
   cookTime: yup.string().required("Tempo de cozimento é obrigatório"),
   servings: yup
     .number()
+    .typeError("Porções deve ser um número")
+    .min(1, "Porções deve ser pelo menos 1")
     .positive("Porções deve ser um número positivo")
     .required("Porções é obrigatório"),
 });
